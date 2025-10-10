302144447a05fa7dde1744150df7182d3dd7897e
Adriel Jacobus Pereira <adrieljp2005@hotmail.com>
2025-10-10 09:18:45 -0300

    Correção de um bug

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 def trocar_entrada():
 	from monitorcontrol import get_monitors 	#pip install monitorcontrol
+	try:
+		with get_monitors()[0] as monitor: #Seleciona o monitor principal passando pra variavel "monitor"
 
-	with get_monitors()[0] as monitor: #Seleciona o monitor principal passando pra variavel "monitor"
+			#VALORES ABAIXO É REFERENCIA AO MEU MONITOR ALIENWARE AW2725DM
+			#NÚMERO 15 = DISPLAYPORT (PC)
+			#NÚMERO 17 = HDMI 1
+			#NÚMERO 18 = HDMI 2 (PS5)
 
-		#VALORES ABAIXO É REFERENCIA AO MEU MONITOR ALIENWARE AW2725DM
-		#NÚMERO 15 = DISPLAYPORT (PC)
-		#NÚMERO 17 = HDMI 1
-		#NÚMERO 18 = HDMI 2 (PS5)
+			ENTRADA_ATUAL = monitor.get_input_source() #Pega a entrada atual do monitor passando pra variavel ENTRADA_ATUAL
 
-		ENTRADA_ATUAL = monitor.get_input_source() #Pega a entrada atual do monitor passando pra variavel ENTRADA_ATUAL
+			if ENTRADA_ATUAL == 15: #Checa se a entrada atual é o DISPLAYPORT
+				monitor.set_input_source(18) #Altera para o HDMI 2
 
-		if ENTRADA_ATUAL == 15: #Checa se a entrada atual é o DISPLAYPORT
-			monitor.set_input_source(18) #Altera para o HDMI 2
+			if ENTRADA_ATUAL == 18: #Checa se a entrada atual é o HDMI 2
+				monitor.set_input_source(15) #Altera para o DISPLAYPORT
 
-		if ENTRADA_ATUAL == 18: #Checa se a entrada atual é o HDMI 2
-			monitor.set_input_source(15) #Altera para o DISPLAYPORT
+	except Exception as e:
+		print("ERRO AO TROCAR A ENTRADA")
+		print(e)
 
 
 def main():
